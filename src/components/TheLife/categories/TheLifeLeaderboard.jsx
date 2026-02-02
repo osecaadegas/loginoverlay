0f2b4df7d9518b79b9015ceb21e531dc83e0c51c
O Seca Adegas <osecaadegas95@gmail.com>
2026-02-02 06:48:17 +0000

    Fix supabaseClient import path

--- src/components/TheLife/categories/TheLifeLeaderboard.jsx
+++ src/components/TheLife/categories/TheLifeLeaderboard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../../../../supabaseClient';
+import { supabase } from '../../../config/supabaseClient';
 import '../styles/TheLifeLeaderboard.css';
 
 const PLAYERS_PER_PAGE = 15;
