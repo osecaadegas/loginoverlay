ab0ed67ff1dcf9148877886947b4e7848441feab
O Seca Adegas <osecaadegas95@gmail.com>
2026-02-19 15:57:19 +0000

    Adjust blackjack betting panel alignment

--- src/components/Blackjack/BlackjackPremium.jsx
+++ src/components/Blackjack/BlackjackPremium.jsx
@@ -561,7 +561,7 @@ export default function BlackjackPremium() {
         </div>
 
         <div className="mt-6 flex justify-center">
-          <div className="w-full max-w-4xl lg:translate-x-32">
+          <div className="w-full max-w-4xl lg:translate-x-38">
             <BettingControls
               gamePhase={gamePhase}
               currentBet={currentBet}
